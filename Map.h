ea11a657f4da4ae2c67a0730b33a282779cbc940
Meghane Decroocq <meghdecroocq@gmail.com>
2017-02-27 14:18:39 +0100

    add Map class

---
+++ Map.h
@@ -0,0 +1,83 @@
+#ifndef MAP_H 
+#define MAP_H 
+
+//==============================
+//    INCLUDES
+//==============================
+
+/**
+ * @class Character
+ * @brief Creates a Character
+ */
+#include<vector>
+
+class Map
+{
+public:
+//==============================
+//    CONSTRUCTORS
+//==============================
+Map();
+
+//==============================
+//    DESTRUCTOR
+//==============================
+virtual ~Map();
+
+//==============================
+//    GETTERS
+//==============================
+  
+
+//==============================
+//    SETTERS
+//==============================
+
+//==============================
+//    OPERATORS
+//==============================
+
+//==============================
+//    PUBLIC METHODS
+//==============================
+void placeBacteries(); // cree et positionne les bacteries 
+void bougeMetabo(); // bouge les metabolites
+void update(); // les bacteries se nourrissent, metabolisent, mutent, se divisent, meurent eventuellement
+void renouvelle(); //reinitialise le milieu de culture
+int* count();// decompte le nombre de bacteries des lignees S et L
+protected:
+//==============================
+//    PROTECTED METHODS
+//==============================
+
+//==============================
+//    ATTRIBUTES
+//==============================
+int width; //longueur grille
+int height;//hauteur grille
+float A_init; //concentration initiale en glucose
+float D; //coef de diffusion
+int T; //temps de simulation
+int t; //temps de renouvellement
+int temps; //temps reel de la simulation
+int h; //pas de temps
+Bacterie* Metaboliste[3][width][height]; //grille des concentrations en metabolites
+std::vector<Bacterie*> listeBacteries(width*height);
+
+
+//==============================
+//    GETTER DEFINITION
+//==============================
+
+
+
+//==============================
+//    SETTER DEFINITION
+//==============================
+
+//==============================
+//    OPERATOR DEFINITION
+//==============================
+};
+#endif // MAP_H
+
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include "Map.h"
+using namespace std;
+
+int main{
+
+  Map grille1();
+
+return 0;
+}
