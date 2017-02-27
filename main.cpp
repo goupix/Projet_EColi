bc412913da8440a1666314a02a39fe4406062f97
goupix <meghdecroocq@gmail.com>
2017-02-27 21:19:38 +0100

    test lignee A

--- main.cpp
+++ main.cpp
@@ -15,14 +15,34 @@ int main(){
   cout<<"Case 1, A: "<< environnement.GetGrille()[0][0].GetA() <<endl;*/
 
 /*Test de la classe Bacterie*/
-
-  Bacterie b=Bacterie();
-  b.Describe();
+  cout<<""<<endl;
+  cout<<"########## Constructeur par défaut de la classe Bactérie ##########"<<endl;
+  cout<<""<<endl;
+  Bacterie t=Bacterie();
+  t.Describe();
 
 /*Test de la classe Lignee_B*/
+  /*Constructeur 1*/
+  cout<<""<<endl;
+  cout<<"########## Bactéries de la lignée B, avec deux constructeurs ##########"<<endl;
+  cout<<""<<endl;
+  Lignee_B b1=Lignee_B(1,3);
+  b1.Describe();
+  /*Constructeur 2*/
+  Lignee_B b2= Lignee_B(1,3,2.5,2.0,1.1);
+  b2.Describe();
 
-  Lignee_B t=Lignee_B(1,3);
-  t.Describe();
+/*Test de la classe Lignee_A*/
+  /*Constructeur 1*/
+  cout<<""<<endl;
+  cout<<"######### Bactéries de la lignée A, avec deux constructeurs ##########"<<endl;
+  cout<<""<<endl;
+  Lignee_A a1=Lignee_A(15,20);
+  a1.Describe();
+  /*Constructeur 2*/
+  Lignee_A a2= Lignee_A(15,20,5.3,0.1,0.6);
+  a2.Describe();
+
 
 
   return 0;
