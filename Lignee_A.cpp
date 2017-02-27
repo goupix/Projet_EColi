ef293d51215a0bb48825bf8a57e599933dffb3da
goupix <meghdecroocq@gmail.com>
2017-02-27 21:17:49 +0100

    construtors, method describe

--- Lignee_A.cpp
+++ Lignee_A.cpp
@@ -17,7 +17,28 @@ using std::endl;
 //    CONSTRUCTORS
 //==============================
 
-Lignee_A::Lignee_A(){}
+Lignee_A::Lignee_A(int x, int y){
+  
+  coord[0]=x;
+  coord[1]=y;
+
+  Raa=0.1;
+  Rab=0.1;
+
+}
+Lignee_A::Lignee_A(int x, int y, float a, float b, float c){
+  
+  coord[0]=x;
+  coord[1]=y;
+
+  A_int=a;
+  B_int=b;
+  C_int=c;
+
+  Raa=0.1;
+  Rab=0.1;
+
+}
 
 
 //==============================
@@ -28,3 +49,9 @@ Lignee_A::~Lignee_A(){}
 //==============================
 //    PUBLIC METHODS
 //==============================
+
+void Lignee_A::Describe(){
+	cout<< "Cette bactérie de type "<<type<<" est placée en ("<<coord[0]<<", "<<coord[1]<<" )"<<endl;
+	cout<< "Elle présente une concentration interne en A: "<<A_int<<", en B: "<<B_int<<" et en C: "<<C_int<<endl<<" ainsi qu'une fitness de "<<w<<endl;
+
+}
